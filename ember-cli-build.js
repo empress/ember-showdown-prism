f07e13c85add4e663032f6d1d513bcf7e24c2508
Chris Manson <chris@manson.ie>
2020-01-14 15:12:52 +0000

    fix tests

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,24 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    'ember-prism': {
+      theme: 'okaidia',
+
+      components: [
+        'apacheconf',
+        'bash',
+        'css',
+        'handlebars',
+        'http',
+        'javascript',
+        'json',
+        'markup-templating',
+        'ruby',
+        'scss'
+      ],
+
+      plugins: ['line-numbers', 'normalize-whitespace']
+    }
   });
 
   /*
