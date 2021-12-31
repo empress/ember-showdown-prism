b99263f4bae7073c53fe418a344941d61e44f809
Chris Manson <chris@manson.ie>
2021-12-31 15:06:21 +0000

    add a basic test

---
+++ tests/acceptance/basic-test.js
@@ -0,0 +1,31 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | index', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('pre.language-none code.language-none').isVisible();
+    assert
+      .dom('pre.language-none code.language-none')
+      .hasText('sh go do a thing');
+
+    assert.dom('pre.language-javascript code.language-javascript').isVisible();
+    assert
+      .dom('pre.language-javascript code.language-javascript')
+      .hasText(`let thing = 'face'; let myElement = $('.my-element');`);
+
+    assert.dom('div.filename .ribbon').isVisible();
+    assert.dom('div.filename span').hasText('app/templates/blog-post.hbs');
+    assert.dom('pre.language-handlebars code.language-handlebars').isVisible();
+    assert
+      .dom('pre.language-handlebars code.language-handlebars')
+      .hasText(
+        `<h1>{{@model.title}}</h1> <h2>by {{@model.author}}</h2> <div class="intro"> {{@model.intro}} </div> <hr> <div class="body"> {{@model.body}} </div>`
+      );
+  });
+});
